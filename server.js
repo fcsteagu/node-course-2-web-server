0f4d4889dce57a4f46383aeb921cb0cce09604a4
Traian Lavric <traian.lavric@gmail.com>
2018-01-04 15:20:39 +0100

    Fix previous commit

--- server.js
+++ server.js
@@ -56,6 +56,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page',
+        projectsMessage: 'Welcome to Projects Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'This is baaad!'
